ca2d463d4cc24892b07d71d8e4b23914bf52ebeb
jaiderbernal90 <jaiderbernal90@gmail.com>
2022-06-11 20:02:29 -0500

    settings front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: 'body',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
   title = 'front';
